7c7e8641b777c431b2f4f7811281ccf2e3913f33
Julius-r <julius.robinson95@yahoo.com>
2020-03-16 23:45:54 -0500

    added components

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Product(props) {
 	return (
 		<article className="product">
-			<p>Product</p>
+			<figure className="product__image">
+				<img src={props.product.image} alt={props.product.name} />
+				<figcaption>{props.product.name}</figcaption>
+			</figure>
+			<section className="price__info"></section>
 		</article>
 	);
 }
